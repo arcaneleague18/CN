9b60cf1cca89678523799b03549e014fea68070b
arcaneleague18 <erenyeager4m@gmail.com>
2025-10-30 23:30:09 +0530

    corrected

--- 03SlidingWindow.java
+++ 03SlidingWindow.java
@@ -3,26 +3,33 @@ import java.util.*;
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter total number of frames: ");
         int frames = sc.nextInt();
+        System.out.print("Enter window size: ");
         int windowSize = sc.nextInt();
         
-        int i = 1;
-        while(i <= frames) {
-            System.out.println("Frames sending");
+        int i = 1; // current frame index
+        
+        while (i <= frames) {
+            System.out.println("\nSending frames...");
             
-            for(int j = i; j < i + windowSize && j <= frames; j++) {
-                System.out.println("sent"+ j + "frame");
+            for (int j = i; j < i + windowSize && j <= frames; j++) {
+                System.out.println("Sent frame " + j);
             }
-            System.out.println("Enter ack: ");
+            
+            System.out.print("Enter ACK for the last successfully received frame: ");
             int ack = sc.nextInt();
             
-            if(ack >=i){
-                i = ack+1;
-                System.out.println("Rolling back to frame"+ack);
+            if (ack >= i && ack < i + windowSize) {
+                i = ack + 1;
+                System.out.println("Sliding window to frame " + i);
             } else {
-                System.out.println("no ack recieved");
+                System.out.println("No valid ACK received. Resending from frame " + i);
             }
         }
-        System.out.println("All frames sent successfully");
+        
+        System.out.println("\nAll frames sent successfully!");
+        sc.close();
     }
 }
